084694b74ef3ed33ccbde0b11ee0efbd92e54292
Ankush523 <ankush.ad050203@gmail.com>
2023-09-13 10:02:54 +0530

    build issue solved

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useRouter } from 'next/router';
 import enviroconnect from "../images/enviroconnect.png";
 
+
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -45,73 +46,78 @@ const Header = () => {
   );
 };
 
-const SignInModal = ({ isOpen, onClose }) => {
-  const router = useRouter();
-  
-  const [signInType, setSignInType] = useState(null); // 'user' or 'authority'
-
-  const handleSignIn = () => {
-    if (signInType === 'user') {
-      // handle user sign-in logic (if any)
-    } else if (signInType === 'authority') {
-      // handle authority sign-in logic (if any)
-    }
-    
-    onClose();
-    router.push('/complain');  // Navigate to the complain page
+type SignInModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
   };
-
-  return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Sign In</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>
-          {!signInType && (
-            <Flex flexDirection="column">
-              <Button mb={3} onClick={() => setSignInType('user')}>Sign in as User</Button>
-              <Button onClick={() => setSignInType('authority')}>Sign in as Authority</Button>
-            </Flex>
-          )}
-
-          {signInType === 'user' && (
-            <>
+  
+  const SignInModal: React.FC<SignInModalProps> = ({ isOpen, onClose }) => {
+    const router = useRouter();
+    
+    const [signInType, setSignInType] = useState<string | null>(null); // 'user' or 'authority'
+  
+    const handleSignIn = () => {
+      if (signInType === 'user') {
+        // handle user sign-in logic (if any)
+      } else if (signInType === 'authority') {
+        // handle authority sign-in logic (if any)
+      }
+      
+      onClose();
+      router.push('/complain');  // Navigate to the complain page
+    };
+  
+    return (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Sign In</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            {!signInType && (
+              <Flex flexDirection="column">
+                <Button mb={3} onClick={() => setSignInType('user')}>Sign in as User</Button>
+                <Button onClick={() => setSignInType('authority')}>Sign in as Authority</Button>
+              </Flex>
+            )}
+  
+            {signInType === 'user' && (
+              <>
+                <FormControl>
+                  <FormLabel>Aadhar Number</FormLabel>
+                  <Input placeholder="Enter Aadhar Number" />
+                </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Phone Number</FormLabel>
+                  <Input placeholder="Enter Phone Number" />
+                </FormControl>
+              </>
+            )}
+  
+            {signInType === 'authority' && (
               <FormControl>
-                <FormLabel>Aadhar Number</FormLabel>
-                <Input placeholder="Enter Aadhar Number" />
+                <FormLabel>Authority ID</FormLabel>
+                <Input placeholder="Enter Authority ID" />
               </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Phone Number</FormLabel>
-                <Input placeholder="Enter Phone Number" />
-              </FormControl>
-            </>
-          )}
-
-          {signInType === 'authority' && (
-            <FormControl>
-              <FormLabel>Authority ID</FormLabel>
-              <Input placeholder="Enter Authority ID" />
-            </FormControl>
-          )}
-        </ModalBody>
-
-        <ModalFooter>
-          {signInType && (
-            <>
-              <Button colorScheme="blue" mr={3} onClick={handleSignIn}> 
-                Sign In
-              </Button>
-              <Button variant="ghost" mr={3} onClick={() => setSignInType(null)}>
-                Back
-              </Button>
-            </>
-          )}
-          <Button onClick={onClose}>Cancel</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  );
-};
-
+            )}
+          </ModalBody>
+  
+          <ModalFooter>
+            {signInType && (
+              <>
+                <Button colorScheme="blue" mr={3} onClick={handleSignIn}> 
+                  Sign In
+                </Button>
+                <Button variant="ghost" mr={3} onClick={() => setSignInType(null)}>
+                  Back
+                </Button>
+              </>
+            )}
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  };
+  
 export default Header;
